85b2c1e5e015d9f4e4c0d1c61f00edfc80c596ff
Rusty Mina <rustymina@gmail.com>
2019-12-01 22:05:28 +0900

    Added PairedDataset structure for pokemon dataset

--- dataset.py
+++ dataset.py
@@ -1,10 +1,48 @@
-# Script to preprocess dataset Taesung Park's repository
-# Turn train and test folder into subfolders
-# torchvision.datasets.ImageFolder only accepts folder with subfolders
 
 import os
 import shutil
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
 
+# Script to loader paired images for aligned dataset
+# Used for pokemon dataset
+# Adapted from https://discuss.pytorch.org/t/how-can-i-read-a-image-pair-at-the-same-time/23151/2?u=rusty
+class PairedDataset(Dataset):
+    def __init__(self, image1_paths, image2_paths, transform=None):
+        
+        A = []
+        B = []
+        for folder in os.listdir(image1_paths):
+            A_folder = os.path.join(image1_paths, folder)
+            B_folder = os.path.join(image2_paths, folder)
+            
+            for image in os.listdir(A_folder):
+                A.append(os.path.join(A_folder, image))
+                B.append(os.path.join(B_folder, image))
+        
+        self.image1_paths = A
+        self.image2_paths = B
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        img1 = Image.open(self.image1_paths[index]).convert("RGB")
+        img2 = Image.open(self.image2_paths[index]).convert("RGB")
+        
+        if self.transform:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            
+        return img1, img2
+    
+    def __len__(self):
+        return len(self.image1_paths)
+
+# Scripts to preprocess dataset Taesung Park's repository
+# Turn train and test folder into subfolders
+# torchvision.datasets.ImageFolder only accepts folder with subfolders
 def prepare_dataset_and_folder(DATASET_PATH, folder_names=[]):
     prepare_dataset(DATASET_PATH)
     prepare_folder(folder_names)
